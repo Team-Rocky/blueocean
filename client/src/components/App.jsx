16047526271b828f03717eb291a4b39d46b27d18
Michael Cruz <mrcruz1989@gmail.com>
2021-05-24 11:24:32 -0400

    fixed git issues. Homepagegrid

--- client/src/components/App.jsx
+++ client/src/components/App.jsx
@@ -1,7 +1,15 @@
-import React from 'react';
+import React, {useState, useCallback, useEffect, useRef} from 'react';
+import Auth from './Auth.jsx';
+import HomePageGrid from './HomePageGrid.jsx'
 
 const App = (props) => {
-return(<div>HELLO</div>)
+
+return(
+  <div>
+    <HomePageGrid/>
+    {/* <Auth updateUser={updateUser}/> */}
+  </div>
+  )
 }
 
 export default App;
--- server/index.js
+++ server/index.js
@@ -1,17 +1,94 @@
 const express = require('express');
-const app = express()
+const fs = require('fs');
+const pug = require('pug');
+const chalk = require('chalk');
 
-app.use(express.json())
-app.use(express.static(__dirname + '/../client/public'));
+// init server
+const app = express();
+const port = 7625;
+app.listen(port, () => { console.log(`listening on port ${port}`) });
+app.use(express.json());
+// logger hits first in chain (on all REQs)
+const getDuration = (start) => {
+  const NS_PER_SEC = 1e9;
+  const NS_TO_MS = 1e6;
+  const diff = process.hrtime(start);
+  return ((diff[0] * NS_PER_SEC + diff[1]) / NS_TO_MS);
+}
+
+app.use((req, res, next) => {
+  const start = process.hrtime();
+  let now = new Date().toISOString();
+  let date = now.slice(0,10);
+  let time = now.slice(11,16);
+  let method = req.method;
+  let url = req.url;
+  res.on('finish', function() {
+    let status = chalk.white(res.statusCode);
+    if (res.statusCode >= 200) { status = chalk.green(res.statusCode); }
+    if (res.statusCode >= 400) { status = chalk.yellow(res.statusCode); }
+    if (res.statusCode >= 500) { status = chalk.red(res.statusCode); }
+    const duration = getDuration(start);
+    let log = `${date} [${time}] - ${method} request to - ${url}\n\t\t     ${status} (${duration}ms)`;
+    console.log(log);
+    //UN-COMMENT
+    // fs.appendFile(`./server/logs/${date}.log`, log, (err) => {
+    //   if (err) throw err;
+    // });
+  });
+  next();
+});
 
 
+// web
+app.use(express.static('./client/public'));
 
-// app.get('/', (req, res) =>{
-//   res.send('hello')
-// })
+// api
+app.use('/api/users', require('./routes/users.js'));
+app.use('/api/recipes', require('./routes/recipes.js'));
 
-let port = 7625
+// error pages
+app.set('view engine', 'pug')
+app.set('views', './server/views');
+app.use(function(req, res) {
+  res.status(404);
+  res.render('404.pug', {title: '404: File Not Found'});
+});
 
-app.listen(port, () => {
-  console.log(`listening on port ${port}`)
-})
+app.use(function(err, req, res, next) {
+  res.status(500);
+  res.render('500.pug', { title: '500: Internal Server error', error: err });
+});
+
+/*
+// http server
+const httpServer = express();
+httpServer.listen(80, () => {
+  console.log('http server listening on port 80');
+});
+httpServer.use((req, res, next) => {
+  console.log(`${req.method} to ${req.protocol}://${req.headers.host}${req.url}`);
+  next();
+});
+httpServer.use((req, res) => {
+  res.redirect(301, `https://${domain-or-ip-address}${req.originalUrl}`);
+});
+
+// https server
+//const https = require('https');
+const ciphers = 'TLS_AES_256_GCM_SHA384:TLS_CHACHA20_POLY1305_SHA256:TLS_AES_128_GCM_SHA256:ECDHE-RSA-AES128-GCM-SHA256:ECDHE-RSA-AES256-GCM-SHA384';
+const app = express();
+https.createServer({
+  key: fs.readFileSync('./server/auth/.server.key'),
+  cert: fs.readFileSync('./server/auth/.server.cert'),
+  ciphers: ciphers,
+  minVersion: 'TLSv1.2'
+}, app).listen(443, () => { console.log('https server listening on port 443'); });
+app.use((req, res, next) => {
+  // only if we want to redirect non-www to www
+  if (req.headers.host.slice(0, 4) !== 'www.') {
+    res.redirect(301, `${domain-or-ip-address}${req.originalUrl}`);
+  }
+  next();
+});
+*/
