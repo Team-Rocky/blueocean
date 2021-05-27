f84d136424f887157cd6f6fdd09ee4e016422df0
Marcia Persaud <marciapersaud@Marcias-MacBook-Pro.local>
2021-05-27 09:39:42 -0400

    update

--- client/src/components/AddToCalendar.jsx
+++ client/src/components/AddToCalendar.jsx
@@ -1,4 +1,7 @@
 import React from 'react';
+import { makeStyles } from '@material-ui/core/styles';
+import { Box, Paper, Button, Modal, TextField } from '@material-ui/core';
+import styled from 'styled-components'
 
 const AddToCalendar = (props) => {
   const days = Object.keys(props.schedule);
@@ -14,21 +17,82 @@ const AddToCalendar = (props) => {
   }));
 
   return (
-    <div>
-      <h2>Weekly Ingredients</h2>
-      <ul>
+    <GroceryList>
+
+      <ListLabel>
+        <div>
+        <img src="https://img.icons8.com/doodle/50/000000/tomato--v1.png"/>
+        GROCERY
+        <img src="https://img.icons8.com/fluent/48/000000/group-of-vegetables.png"/>
+        </div>
+        <List>List</List>
+
+        </ListLabel>
+      <Weekly>
         {days.map((day, index) => (
-            <li key={index} style={{listStyle: 'none'}}>
-              <h3>{day}</h3>
+            <DailyList key={index} style={{listStyle: 'none'}}>
+              <Day>{day}</Day>
               <ul>
                 {ingredients[day].map((ingredients, index) => ingredients.map((ingredient, index) =>
                 <li key={index}>{ingredient}</li>))}
               </ul>
-            </li>
+            </DailyList>
         ))}
-      </ul>
-    </div>
+      </Weekly>
+    </GroceryList >
   );
 };
+
+const List = styled.div`
+  border-radius: 0.5em;
+  margin: 0;
+  padding: 0.2em;
+  color: white;
+  width: 25%;
+  background-color: #20b2aa;
+`
+
+
+const ListLabel = styled.h2`
+  margin: 1em;
+  font-family: 'Architects Daughter',cursive;
+  font-size: 40px;
+  display: flex;
+  flex-direction: column;
+  align-items: center;
+`
+
+const GroceryList = styled.div`
+  display: flex;
+  flex-direction: column;
+  align-items: center;
+  background-color: #dbb572;
+  background-image: url("https://www.transparenttextures.com/patterns/little-pluses.png");
+
+`
+const Day = styled.div`
+  text-align: center;
+  padding-top: 0.5em;
+  color: #dc911e;
+
+`
+
+const DailyList = styled.li`
+  background-color: white;
+  border: 1px solid black;
+  margin: 1em;
+  height: 300px;
+  border-radius: 0.3em;
+  border-style: none;
+`
+
+const Weekly = styled.ul`
+  display: grid;
+  width: 100%;
+  grid-template-columns: 25% 25% 25% 25%;
+  padding: 0;
+  margin: 0;
+
+`
 
 export default AddToCalendar;
